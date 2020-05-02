65907747ac63e77a4477d80c0029b3fcee4330fc
Lucas <55569386+Blacklinkker@users.noreply.github.com>
2020-05-01 23:16:24 -0300

    Determines a pixel's color
    Part of a project of mine, basically it takes a single pixel and analise it colors, returns RGB values

---
+++ PixelColorBot.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.awt.*;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+public class PixelColorBot{
+    public static void main(String args[]) throws IOException{
+        File file= new File("image.png");
+        BufferedImage image = ImageIO.read(file);
+        // Getting pixel color by position x and y 
+
+        int clr=  image.getRGB(1000,1000); 
+        int  red   = (clr & 0x00ff0000) >> 16;
+        int  green = (clr & 0x0000ff00) >> 8;
+        int  blue  =  clr & 0x000000ff;
+        System.out.println("Red Color value = "+ red);
+        System.out.println("Green Color value = "+ green);
+        System.out.println("Blue Color value = "+ blue);
+
+    }
+}   
+    
