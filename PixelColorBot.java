c5bc13176c43213aa28b8e3219259665be3b2ede
Lucas <55569386+Blacklinkker@users.noreply.github.com>
2020-05-01 23:18:17 -0300

    Update PixelColorBot.java

--- PixelColorBot.java
+++ PixelColorBot.java
@@ -1,13 +1,16 @@
+//probably i imported a lot more than necessary 
 import java.io.*;
 import java.awt.*;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 public class PixelColorBot{
     public static void main(String args[]) throws IOException{
+        //used a local image on this run
         File file= new File("image.png");
         BufferedImage image = ImageIO.read(file);
-        // Getting pixel color by position x and y 
-
+        
+        //select the pixel position manually
+        //x,y
         int clr=  image.getRGB(1000,1000); 
         int  red   = (clr & 0x00ff0000) >> 16;
         int  green = (clr & 0x0000ff00) >> 8;
