3a7f52ad3a6125ac3fa3d4a9de9700f2d6c5d805
Lucas <55569386+LucasMonir@users.noreply.github.com>
2022-01-08 22:27:32 -0300

    Indentation fixed

--- KeyBotTest.java
+++ KeyBotTest.java
@@ -2,12 +2,11 @@ import java.awt.Robot;
 import java.awt.AWTException;
 import java.awt.event.MouseEvent;
 import java.awt.event.KeyEvent;
+
 public class KeyBotTest {
-    
-    //the clicking method
     public static void clicker(int x,int y){
-        Robot robot2;
         try {
+            Robot robot2;
             robot2 = new Robot();
             robot2.mouseMove(x, y);
             robot2.delay(100);
@@ -15,13 +14,12 @@ public class KeyBotTest {
             robot2.mouseRelease(MouseEvent.BUTTON1_MASK);
             robot2.delay(500);
         } catch (AWTException e) {
-            
             e.printStackTrace();
         }
     }
+
     public static void main(String[] args) {
         try {
-            
             Robot robot = new Robot();
             clicker(1000, 950);
             robot.keyPress(KeyEvent.VK_CONTROL);
@@ -30,13 +28,8 @@ public class KeyBotTest {
             robot.keyRelease(KeyEvent.VK_CONTROL);
             robot.delay(100);
             clicker(1875,1000);
-           
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-              
-        
     }    
-
 }
